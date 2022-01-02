4b61df439f1f268166a21cad913ceb10704cbb00
azhansyakirin <96921398+azhansyakirin@users.noreply.github.com>
2022-01-02 16:21:25 +0800

    Add files via upload

---
+++ isslocator/tweet.php
@@ -0,0 +1,5 @@
+	<div id="container">
+
+		<iframe style="border:none;" height="400" data-tweet-url="https://twitter.com/Space_Station" src="data:text/html;charset=utf-8,%3Ca%20class%3D%22twitter-timeline%22%20href%3D%22https%3A//twitter.com/Space_Station%3Fref_src%3Dtwsrc%255Etfw%22%3ETweets%20by%20Space_Station%3C/a%3E%0A%3Cscript%20async%20src%3D%22https%3A//platform.twitter.com/widgets.js%22%20charset%3D%22utf-8%22%3E%3C/script%3E%0A"></iframe>
+
+	</div>
